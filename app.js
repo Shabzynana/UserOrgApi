9ac4eeb41300dfe20f5a212b38d6fe2df849ed13
MAC <horlarmihleykan10@gmail.com>
2024-07-05 11:47:00 +0100

    boiler plate

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const session = require('express-session');
+// const cookieParser = require('cookie-parser');
+// const cors = require('cors');
+// require('dotenv').config();
+const path = require('path');
+
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, './src/templates'))
+
+// Render login page
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+
+app.use(bodyParser.json());
+// app.use(cors());
+// app.use(cookieParser())
+
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+});
